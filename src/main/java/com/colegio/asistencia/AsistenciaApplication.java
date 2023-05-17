44752bd03ba3ead5c2d5fd9f45c86a217f85f1f2
JaiderMartinez <smithsaemirto45@gmail.com>
2023-05-16 20:32:46 -0500

    first commit

---
+++ src/main/java/com/colegio/asistencia/AsistenciaApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.asistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/colegio/asistencia/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
